d8370bbe8009fa846092a4f6231d976bcea7bc68
Gustavo Londono <tavon321@gmail.com>
2021-02-01 11:34:55 -0500

    starts twice route to first question twice

--- Core/CoreTests/FlowTests.swift
+++ Core/CoreTests/FlowTests.swift
@@ -54,6 +54,16 @@ class FlowTests: XCTestCase {
         XCTAssertEqual(router.routerQuestions, [expectedQuestion])
     }
 
+    func test_startTwice_withTwoQuestionRouteToFirstQuestionTwice() {
+        let expectedQuestion = "a question"
+        let expectedQuestionTwo = "another question"
+        let (sut, router) = makeSUT(questions: [expectedQuestion, expectedQuestionTwo])
+
+        sut.start()
+
+        XCTAssertEqual(router.routerQuestions, [expectedQuestion])
+    }
+
     // MARK: - Helpers
     private func makeSUT(questions: [String] = []) -> (sut: Flow, router: RouterSpy){
         let router = RouterSpy()
